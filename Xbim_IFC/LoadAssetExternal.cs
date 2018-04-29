0222a49f7e748af7477c6fb1bc0068a38c18fffa
luoyuke <luoyuke911123@gmail.com>
2018-04-29 14:42:53 +0200

    Add files via upload

---
+++ Xbim_IFC/LoadAssetExternal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetExternal : MonoBehaviour {
+
+    private string FBX_path = "C:/Users/luoyu/Documents/Revit_project/2018_03_Revit_Fbxport";
+    private string FBX_name = "BuildingFbx_3ds.fbx";
+
+    // Use this for initialization
+    void Start ()
+    {
+        /* load AssetBundle
+        AssetBundle bundle = AssetBundle.LoadFromFile(FBX_path);
+        GameObject building = Instantiate(bundle.LoadAsset<GameObject>(FBX_name)) as GameObject;
+        Debug.Log("load:" + bundle.ToString());
+        */
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Xbim_IFC/LoadAssetIFC.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+
+public class LoadAssetIFC : MonoBehaviour {
+    const string IFCFile = "IFCBuilding.ifc";
+	// Use this for initialization
+	void Start () {
+        var editor = new XbimEditorCredentials
+        {
+            ApplicationDevelopersName = "yuke",
+            ApplicationFullName = "ifc_to_unity",
+            ApplicationIdentifier = "Your app ID",
+            ApplicationVersion = "4.0",
+            //your user
+            EditorsFamilyName = "Luo",
+            EditorsGivenName = "Yuke",
+            EditorsOrganisationName = "Educational study"
+        };
+
+        using (var model = IfcStore.Open(IFCFile))
+        {
+            var IFCProducts = model.Instances.OfType<IIfcProduct>();
+            foreach (var ifcProduct in IFCProducts)
+            {
+                Debug.Log("ifcproductexpresstype: " + ifcProduct.ExpressType + " ,productlabel: " + ifcProduct.ObjectType +" ,productintID: " + ifcProduct.EntityLabel);
+                
+                if (ifcProduct.ExpressType.ToString() == "IfcSpace")
+                {
+                    GameObject go = GameObject.Find(ifcProduct.EntityLabel.ToString());
+                    go.SetActive(false);
+                }
+                      
+                
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
